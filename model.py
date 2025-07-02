a47bd0e01b70ee670393ce19f84e0b071b2ef140
Egie2 <egieamilia@gmail.com>
2025-07-02 23:12:00 +0700

    Delete model.py

--- model.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-class Tugas:
-    def __init__(self, judul, deskripsi, deadline, status="Belum Selesai", id_tugas=None):
-        self.id = id_tugas
-        self.judul = judul or "Tanpa Judul"
-        self.deskripsi = deskripsi or "-"
-        self.deadline = deadline if isinstance(deadline, datetime.date) else datetime.date.today()
-        self.status = status
